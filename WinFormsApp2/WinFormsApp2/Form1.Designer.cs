ceefc2eeb6b1e608a8c8fac4be8ce2d17082b66a
GeogeKhodaev <74020503+GeogeKhodaev@users.noreply.github.com>
2021-04-26 18:08:09 +0300

    Add files via upload

---
+++ WinFormsApp2/WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::WinFormsApp2.Properties.Resources.picture1;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(637, 501);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 74);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.button1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button1_KeyUp);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::WinFormsApp2.Properties.Resources.Beam;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(638, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 240);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 18000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1063, 552);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(945, 552);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "0";
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1245, 511);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Пауза";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1245, 552);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1065, 512);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 34);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Старт";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(971, 522);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Очки";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1402, 587);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Игра";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinFormsApp2/WinFormsApp2/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                button2.Location = new Point(button2.Location.X - 5, button2.Location.Y);
+                button1.Location = new Point(button1.Location.X - 5, button1.Location.Y);
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                button2.Location = new Point(button2.Location.X + 5, button2.Location.Y);
+                button1.Location = new Point(button1.Location.X + 5, button1.Location.Y);
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                button2.Visible = false;
+            }
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                button2.Location = new Point(button2.Location.X - 5, button2.Location.Y);
+                button1.Location = new Point(button1.Location.X - 5, button1.Location.Y);
+            }
+
+            else if (e.KeyCode == Keys.D)
+            {
+                button2.Location = new Point(button2.Location.X + 5, button2.Location.Y);
+                button1.Location = new Point(button1.Location.X + 5, button1.Location.Y);
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                button2.Visible = true;
+            }
+        }
+        Button button;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            button = new Button();
+            button.Location = new Point(r.Next(16, 1060), 0);
+            button.BackgroundImage = WinFormsApp2.Properties.Resources.Meteor;
+            button.BackgroundImageLayout = ImageLayout.Zoom;
+            button.Size = new Size(98, 95);
+            this.Controls.Add(button);
+            
+        }
+        int a = 0;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+            
+
+            foreach (Control c in this.Controls)
+            {
+                if (c.Name != "button1" && c.Name != "button2"&& c.Name != "button3" && c.Name != "button4" )
+                {
+                    if (c is Button)
+                    {
+                        ((Button)c).Location = new Point(c.Location.X, c.Location.Y + 5);
+
+                    }
+
+                    if (c.Location.X/10 == button2.Location.X/10&&c.Location.Y/10==button2.Location.Y/10&&button2.Visible==true)
+                    {
+                        a=a+1;
+                        this.Controls.Remove(c);
+                        textBox2.Text = Convert.ToString(a);
+                    }
+                    if (c.Location.X / 10 == button1.Location.X / 10 && c.Location.Y / 10 == button1.Location.Y / 10 && button1.Visible == true)
+                    {
+                        c.Visible = false;
+                        button1.Visible = false; 
+                        timer1.Enabled = false;
+                        timer2.Enabled = false;
+                        timer3.Enabled = false; 
+                        button = new Button();
+                        button.Location = new Point(button1.Location.X, button1.Location.Y);
+                        button.BackgroundImage = WinFormsApp2.Properties.Resources.exp;
+                        button.BackgroundImageLayout = ImageLayout.Zoom;
+                        button.Size = new Size(98, 95);
+                        this.Controls.Add(button);
+                        MessageBox.Show("Ты проиграл");
+                        this.Close();
+                        
+                    }
+                }
+
+            }
+            //this.Controls.Remove(button);
+
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(button2.Location.X/10);
+
+            if (button != null)
+                textBox3.Text = Convert.ToString(button.Location.X / 10);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button1.Visible=true;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+        }
+    }
+}
